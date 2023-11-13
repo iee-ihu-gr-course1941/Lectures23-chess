c94512f1eeb1bda326aec3bf67411f23f4af168c
Antonis Sidiropoulos <asidirop@gmail.com>
2023-11-13 10:00:18 +0200

    creation of chess.php

---
+++ chess.php
@@ -0,0 +1,17 @@
+<?php
+
+print "HERE!!!!!!!!!!";
+# print "<pre>";
+# print_r($_SERVER);
+# print "</pre>";
+
+$method = $_SERVER['REQUEST_METHOD'];
+$request = explode('/', trim($_SERVER['PATH_INFO'],'/'));
+// $request = explode('/', trim($_SERVER['SCRIPT_NAME'],'/'));
+// Σε περίπτωση που τρέχουμε php –S 
+$input = json_decode(file_get_contents('php://input'),true);
+
+ print_r($request );
+exit;
+
+?>
